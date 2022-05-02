f10ce6845af76a4b80d0eec4a3ec44bfff817dad
nicola.fiorentino <nicola.fiorentino@moveo.it>
2022-05-02 10:03:23 +0200

    'ciao'

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,4 @@
-import org.apache.commons.lang3.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by jt on 2018-11-26.
@@ -7,6 +7,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!! ");
-        System.out.println(StringUtils.capitalize("hello world"));
+  //      System.out.println(StringUtils.capitalize("hello world"));
     }
 }
